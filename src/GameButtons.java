5c6ee97719b55071ec919ac85e5cb517be0047b7
KareemNader <85237859+KareemNader@users.noreply.github.com>
2023-06-12 18:14:11 +0300

    Update GameButtons.java

--- src/GameButtons.java
+++ src/GameButtons.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package game;
 
 import javax.swing.*;
@@ -9,10 +5,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-/**
- *
- * @author ProRandom
- */
+
+
 public class gameButtons extends JButton implements ActionListener {
     private int row ;
     private int col ;
